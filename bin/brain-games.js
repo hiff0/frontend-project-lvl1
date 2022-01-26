f368f19d68f1a68b662b713c52bfe8c0bc27fbc0
Петров Владислав <uservlad@LAPTOP-6UG95S0N.localdomain>
2022-01-26 14:52:51 +0300

    add user input

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import { Name } from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
+Name();
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const Name = () => {
+    const name = readlineSync.question('What is your name?');
+    console.log(`Hello, ${name}`);
+}
