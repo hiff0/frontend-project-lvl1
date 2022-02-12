eae33c43da7ee16af876f552382f02cf653680e0
Vlad Petrov <hiffchanel@gmail.com>
2022-02-12 22:54:43 +0300

    change logic game

--- src/games/games-logic.js
+++ src/games/games-logic.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export const Name = () => {
 
     console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May i have yout name?');
+    const name = readlineSync.question('May i have yout name? ');
     console.log(`Hello, ${name}`);
 }
