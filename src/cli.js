5bec041780fb33df4cdb9b9b702bc36f4f71c09f
Vlad Petrov <hiffchanel@gmail.com>
2022-03-29 02:02:07 +0300

    fix logic

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const Name = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May i have yout name?');
   console.log(`Hello, ${name}`);
 };
