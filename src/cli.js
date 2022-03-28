311572d27a762e2309a11cb5c13c7c5cef6a397b
Vlad Petrov <hiffchanel@gmail.com>
2022-03-29 02:06:13 +0300

    bug fix

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const Name = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May i have yout name?');
+  const name = readlineSync.question('May i have yout name? ');
   console.log(`Hello, ${name}`);
 };
 export default Name;
