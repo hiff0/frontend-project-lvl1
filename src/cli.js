a3a61394a7463efd9a6cfc9385f010ae8a91b16f
Vlad Petrov <hiffchanel@gmail.com>
2022-03-30 15:42:07 +0300

    typo cerrection

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const Name = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May i have yout name? ');
+  const name = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${name}`);
 };
 export default Name;
